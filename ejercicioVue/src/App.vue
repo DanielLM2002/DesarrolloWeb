42820d514e338a79f6cedf710fe942e9402c050b
Daniel Lizano <dlizano@outlook.com>
2023-10-07 22:31:43 -0600

    ejercicio vue

--- ejercicioVue/src/App.vue
+++ ejercicioVue/src/App.vue
@@ -1,30 +1,86 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+    name: 'app',
+    data() {
+    return {
+      name: "Daniel",
+      tasks: [],
+      hideCompleted: true,
+      newItemText: ""
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({ action: this.newItemText, done: false });
+      this.storeData();
+      this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data != null) {
+      this.tasks = JSON.parse(data);
+    }
+  }
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+   <h4 class="bg-primary text-white text-center p-2">
+     {{ name }}'s To Do List
+   </h4>
+   <div class="container-fluid p-4">
+     <div class="row" v-if="filteredTasks.length == 0">
+       <div class="col text-center">
+         <b>Nothing to do. Hurrah!</b>
+       </div>
+     </div>
+     <template v-else>
+       <div class="row">
+         <div class="col fw-bold">Task</div>
+         <div class="col-2 fw-bold text-center">Done</div>
+       </div>
+       <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+         <div class="col">{{ t.action }}</div>
+         <div class="col-2 text-center">
+           <input type="checkbox" v-model="t.done" class="form-check-input" />
+         </div>
+       </div>
+     </template>
+     <div class="row py-2">
+       <div class="col w-75">
+         <input v-model="newItemText" class="form-control" />
+       </div>
+       <div class="col-2">
+         <button class="btn btn-primary w-100" v-on:click="addNewTodo">Add</button>
+       </div>
+     </div>
+     <div class="row bg-secondary py-2 mt-2 text-white">
+       <div class="col text-center">
+         <input type="checkbox" v-model="hideCompleted" class="form-check-input mx-2 my-2" />
+         <label class="form-check-label fw-bold my-1">
+           Hide completed tasks
+         </label>
+       </div>
+       <div class="col text-center">
+         <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+           Delete Completed
+         </button>
+       </div>
+     </div>
+   </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
